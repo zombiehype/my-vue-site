1643071bab5b463c631314b25f578a5906ae907c
Riccardo Farina <riccardo.farina@gmail.com>
2025-08-20 22:07:03 +0200

    Initial Vue + CMS setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const page = ref(null)
+
+onMounted(async () => {
+  const res = await fetch('/src/content/pages/home.md')
+  page.value = await res.text()
+})
+</script>
+
+<template>
+  <main>
+    <h1>Home Page</h1>
+    <div v-if="page">
+      <pre>{{ page }}</pre>
+    </div>
+  </main>
+</template>
